6fa7ae47c3a53b8eaa606d62383fe093274561e2
Chad Fusco <fusco@case.edu>
2023-03-17 18:39:58 -0700

    added comments.js export to models/index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 exports.users = require('./users/users');
 exports.spaces = require('./spaces/spaces');
 exports.confessions = require('./confessions/confessions');
+exports.comments = require('./comments/comments');
